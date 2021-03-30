f4eb026a42018817e681befc722d4a5654495275
muhammad-shahroze <muhammad.shahroze@outlook.com>
2021-03-30 13:06:41 +0100

    Added j-query and started coding PHP for Earthquakes API

---
+++ task/libs/php/getGeoEarthquakesInfo.php
@@ -0,0 +1,12 @@
+<?php
+
+	ini_set('display_errors', 'On');
+	error_reporting(E_ALL);
+
+	$executionStartTime = microtime(true);
+
+  //ref url = http://api.geonames.org/earthquakesJSON?formatted=true&north=44.1&south=-9.9&east=-22.4&west=55.2&username=muhammad_shahroze&style=full
+
+  $url='http://api.geonames.org/earthquakesJSON?formatted=true&north='
+
+?>
